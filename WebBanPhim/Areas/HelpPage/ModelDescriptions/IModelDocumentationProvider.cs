b6092905a2aeb33601d5ad4ed69249523f37cbf8
Tiến Đạt <nguyendat.wk@gmail.com>
2023-03-08 08:32:57 +0700

    upload

---
+++ WebBanPhim/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace WebBanPhim.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
